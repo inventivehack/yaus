5f5318ad1f37e1c97318276992e0805dfd76b903
Pablo Trinidad <pabloatp0204@gmail.com>
2016-07-02 19:33:08 -0500

    Link tests and redirection

--- links/views.py
+++ links/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import RedirectView
 
-# Create your views here.
+from .models import Link
+
+
+class URLRedirectView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        url = get_object_or_404(Link, url=kwargs.get('url', None))
+        url.hits += 1
+        url.save()
+        return url.redirect_url
